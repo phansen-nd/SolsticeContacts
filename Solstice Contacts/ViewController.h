e8af65de115134ec028726b373db56eb5c30471b
Pat <patrick.hansen1618@gmail.com>
2015-07-22 17:50:46 -0700

    Initial Commit

---
+++ Solstice Contacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Solstice Contacts
+//
+//  Created by Patrick on 7/22/15.
+//  Copyright (c) 2015 Patrick Hansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Solstice Contacts/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Solstice Contacts
+//
+//  Created by Patrick on 7/22/15.
+//  Copyright (c) 2015 Patrick Hansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
